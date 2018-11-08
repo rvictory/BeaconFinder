f04ce59c3df26c5bfd2f77feaa6ca1245fbc9b3a
Punch Plus Plus <plusplus@roadhou.se>
2018-11-08 23:09:30 +0200

    Should always run the tests before you commit...

--- tests/test_bro_file.rb
+++ tests/test_bro_file.rb
@@ -26,7 +26,7 @@ class TestMockChannelManager < MiniTest::Test
 
   def test_can_extract_line
     record = @bro_file.get_line(1)
-    assert_equal("1460742445.389561", record[0].data, "Didn't extract ts properly")
+    assert_equal("1460742445.389561", record[0], "Didn't extract ts properly")
 
     record = @bro_file.get_line(1000)
     assert_nil(record, "Didn't get an empty line when should have")
@@ -35,9 +35,9 @@ class TestMockChannelManager < MiniTest::Test
   def test_can_read_sequentially
     @bro_file.open!
     next_record = @bro_file.next_record
-    assert_equal("1460742445.389561", next_record[0].data, "Didn't extract ts properly")
+    assert_equal("1460742445.389561", next_record[0], "Didn't extract ts properly")
     next_record = @bro_file.next_record
-    assert_equal("1460742499.633776", next_record[0].data, "Didn't extract ts properly")
+    assert_equal("1460742499.633776", next_record[0], "Didn't extract ts properly")
     1000.times {@bro_file.next_record}
     next_record = @bro_file.next_record
     assert_nil(next_record, "Didn't return nil when no more lines were left")
